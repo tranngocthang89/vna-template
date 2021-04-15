4f9be93ea289a3b4b53082ad0ec68e533acb0ca6
zeno <tranngocthang89@gmail.com>
2021-04-15 14:11:57 +0700

    category

--- js/app.js
+++ js/app.js
@@ -10,3 +10,21 @@ var swiper = new Swiper('.feature-product-swiper', {
         prevEl: '.button-swiper-home-prev',
     },
 });
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
